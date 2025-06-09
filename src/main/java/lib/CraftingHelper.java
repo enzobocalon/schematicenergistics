bb7872ae180123ac58ba5807da0398f58aae50a6
Enzo Bocalon <enzo.bocalon@hotmail.com>
2025-06-09 20:51:15 -0300

    feat: changes request logic to AE2 methods

--- src/main/java/lib/CraftingHelper.java
+++ src/main/java/lib/CraftingHelper.java
@@ -1,6 +1,7 @@
 package lib;
 
 import appeng.api.networking.crafting.CalculationStrategy;
+import appeng.api.networking.crafting.ICraftingLink;
 import appeng.api.networking.crafting.ICraftingPlan;
 import appeng.api.networking.crafting.ICraftingService;
 import appeng.api.stacks.AEItemKey;
@@ -12,27 +13,52 @@ import net.minecraft.world.level.Level;
 public class CraftingHelper {
     private CraftingRequest pendingCraft;
     private final CannonInterfaceEntity entity;
+    private ICraftingLink link;
 
     public CraftingHelper(CannonInterfaceEntity entity) {
         this.entity = entity;
     }
 
     public void startCraft(AEItemKey key, long amount) {
-        ICraftingService service = (Objects.requireNonNull(this.entity.getGridNode())).getGrid().getCraftingService();
-        if (key != null && amount > 0) {
-            if (service.isCraftable(key)) {
-                Level level = this.entity.getLevel();
-                CannonInterfaceEntity entity = this.entity;
-                Future<ICraftingPlan> future = service.beginCraftingCalculation(level, entity::getActionSource, key, amount, CalculationStrategy.REPORT_MISSING_ITEMS);
-                if (future != null) {
-                    this.pendingCraft = new CraftingRequest(key, amount, future);
-                }
-            }
+        if (key == null || amount <= 0 || this.entity.getLevel() == null) {
+            return;
+        }
+
+        var node = this.entity.getGridNode();
+        if (node == null) return;
+
+        var grid = node.getGrid();
+        if (grid == null) return;
+
+        var service = grid.getCraftingService();
+        if (!service.isCraftable(key)) {
+            return;
+        }
+
+        var future = service.beginCraftingCalculation(
+                this.entity.getLevel(),
+                entity::getActionSource,
+                key,
+                amount,
+                CalculationStrategy.REPORT_MISSING_ITEMS
+        );
+
+        if (future != null) {
+            this.pendingCraft = new CraftingRequest(key, amount, future);
         }
     }
 
+
     public void clearPendingCraft() {
         this.pendingCraft = null;
+    }
+
+    public void setLink(ICraftingLink link) {
+        this.link = link;
+    }
+
+    public ICraftingLink getLink() {
+        return this.link;
     }
 
     public CraftingRequest getPendingCraft() {
